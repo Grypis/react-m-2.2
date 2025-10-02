94727eae0fbbf123ac8c9488bde29a344a1127cf
grypis <grypisnecros@gmail.com>
2025-10-02 19:05:01 +0300

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,64 @@ import Profile from './components/Profile/Profile';
 import Section from './components/Section/Section';
 
 import dataFromServer from './db/profiles.json';
+import Bar from './components/Bar/Bar';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const incrementCounter = () => {
-    // setCounter(counter + 1);
-    // setCounter(counter + 1);
-    // setCounter(counter + 1); ❌
-    // setCounter(actualCounter => actualCounter + 1);
-    // setCounter(actualCounter => actualCounter + 1);
-    // setCounter(actualCounter => actualCounter + 1); ✅
-  };
+  const [showUserList, setShowUserList] = useState(false);
+  const [bottles, setBottles] = useState({
+    beer: 2,
+    wine: 3,
+    whiskey: 1,
+  });
 
   const handleClisk = userName => {
     console.log('userName :', userName);
   };
 
+  const toggleUserList = () => {
+    // setShowUserList(actualState => !actualState);
+    setShowUserList(!showUserList);
+  };
+
+  const onBarSupplyAdd = alcoName => {
+    console.log('click', alcoName);
+  };
+
+  const total = bottles.beer + bottles.wine + bottles.whiskey;
+
   return (
     <div>
-      <Section>
-        <p>Counter: {counter}</p>
-        <button type="button" onClick={incrementCounter}>
-          Count
-        </button>
+      <Section title="FSON105 weekend bar">
+        <Bar
+          beer={bottles.beer}
+          wine={bottles.wine}
+          whiskey={bottles.whiskey}
+          total={total}
+          onBarSupplyAdd={onBarSupplyAdd}
+        />
       </Section>
-
       <Section>
-        {dataFromServer.map(
-          ({ email, name, phone, status, hasPhisicalAddress }) => {
-            return (
-              <Profile
-                key={email}
-                name={name}
-                phone={phone}
-                email={email}
-                status={status}
-                hasPhisicalAddress={hasPhisicalAddress}
-                handleClisk={handleClisk}
-              />
-            );
-          },
+        <button type="button" onClick={toggleUserList}>
+          Toggle User List
+        </button>
+        {showUserList && (
+          <div>
+            {dataFromServer.map(
+              ({ email, name, phone, status, hasPhisicalAddress }) => {
+                return (
+                  <Profile
+                    key={email}
+                    name={name}
+                    phone={phone}
+                    email={email}
+                    status={status}
+                    hasPhisicalAddress={hasPhisicalAddress}
+                    handleClisk={handleClisk}
+                  />
+                );
+              },
+            )}
+          </div>
         )}
       </Section>
     </div>
